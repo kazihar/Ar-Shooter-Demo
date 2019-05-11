45201e26b8f6ff7987c76c022a6947eb70b73d12
kazihar Cannei <kaziharkanneth@gmail.com>
2019-05-11 23:53:32 +0530

    Add files via upload

---
+++ Gun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void shoot()
+    {
+     GameObject go = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+     go.GetComponent<Rigidbody>().AddForce(transform.forward * 1500, ForceMode.Impulse);
+     GameObject co = Instantiate(explosion, transform.position, Quaternion.LookRotation(transform.forward));
+     Destroy(go, 5f);
+     }
+
+    
+}
